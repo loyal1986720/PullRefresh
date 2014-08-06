973524ce968b23dadc85c2d65324d024cef3ef0d
loyal1986720 <83272388@qq.com>
2014-08-06 17:37:34 +0800

    xcode6 beta5  参数调整

--- PullRefresh/Refresh/RefreshHeaderView.swift
+++ PullRefresh/Refresh/RefreshHeaderView.swift
@@ -85,7 +85,7 @@ class RefreshHeaderView: RefreshBaseView {
     }
     
     //监听UIScrollView的contentOffset属性
-    override func observeValueForKeyPath(keyPath: String!, ofObject object: AnyObject!, change: [NSObject : AnyObject]!, context: UnsafePointer<()>) {
+    override func observeValueForKeyPath(keyPath: String!, ofObject object: AnyObject!, change: [NSObject : AnyObject]!, context: UnsafeMutablePointer<()>) {
         if (!self.userInteractionEnabled || self.hidden){
             return
         }
